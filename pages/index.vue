3d0352a22914cc3520472a8ba4ec8af31ced2258
ocinpp <ocin25@gmail.com>
2020-07-20 04:50:29 +0800

    Use grid instead of table

--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,34 @@
 <template>
   <div class="pa4">
     <div class="w-100 mw8 center">
-      <h1 class="f4 white pv2 ph3">Hong Kong Weather Forecast</h1>
-      <div class="pa3 bt bb b--dark-blue">
-        <p class="f6 f5-ns lh-copy mv4 white">{{ generalSituation }}</p>
+      <h1 class="f4 white pv2 ph3">{{ title }}</h1>
+      <div class="ph3 bt b--purple">
+        <p class="f5 lh-copy mv4 white">{{ generalSituation }}</p>
       </div>
-      <div class="overflow-auto">
-        <table class="f6 w-100" cellspacing="0">
-          <tbody class="lh-copy white">
-            <tr
-              v-for="weatherForecast of weatherForecasts"
-              :key="weatherForecast.forecastDate"
-            >
-              <td class="pa3 bb b--dark-blue">
-                {{ weatherForecast.forecastDate | formatDate }}
-              </td>
-              <td class="pa3 bb b--dark-blue">{{ weatherForecast.week }}</td>
-              <td class="pa3 bb b--dark-blue">
-                {{ weatherForecast.forecastWeather }}
-              </td>
-            </tr>
-          </tbody>
-        </table>
+      <div>
+        <h2 class="f5 white pv2 ph3">{{ forecast }}</h2>
+      </div>
+      <div>
+        <div
+          v-for="weatherForecast of weatherForecasts"
+          :key="weatherForecast.forecastDate"
+          class="mw9 center ph3-ns white bt b--dark-pink"
+        >
+          <div class="cf ph2-ns">
+            <div class="fl w-50 w-20-m w-20-l pa3 word-wrap">
+              {{ weatherForecast.forecastDate | formatDate }}
+            </div>
+            <div class="fl w-50 w-30-m w-20-l pa3 word-wrap">
+              {{ weatherForecast.forecastMintemp.value }}
+              {{ weatherForecast.forecastMintemp.unit }} /
+              {{ weatherForecast.forecastMaxtemp.value }}
+              {{ weatherForecast.forecastMaxtemp.unit }}
+            </div>
+            <div class="fl w-100 w-50-m w-60-l pa3 word-wrap">
+              {{ weatherForecast.forecastWeather }}
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -33,11 +40,9 @@ export default {
     // called every time before loading the component
     // as the name said, it can be async
     // Also, the returned object will be merged with your data object
-
     const res = await $axios.get(
       'https://data.weather.gov.hk/weatherAPI/opendata/weather.php?dataType=fnd&lang=en'
     )
-    // const data = res.data
     return {
       generalSituation: res.data.generalSituation,
       weatherForecasts: res.data.weatherForecast,
@@ -45,7 +50,8 @@ export default {
   },
   data() {
     return {
-      hello: 'world',
+      title: 'Hong Kong Weather Forecast',
+      forecast: '9 Day Forecast',
     }
   },
 }
