65e1a319a1ad004d7bcd16f52ed3011d5832439c
ocinpp <ocin25@gmail.com>
2020-07-20 20:32:20 +0800

    Add ISO Date parse and format

--- plugins/filters.js
+++ plugins/filters.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
 import parse from 'date-fns/parse'
+import parseISO from 'date-fns/parseISO'
 import format from 'date-fns/format'
 
 Vue.filter('capitalize', (val) => val.toUpperCase())
 
 Vue.filter('formatDate', (val) =>
   format(parse(val, 'yyyyMMdd', new Date()), 'dd-MMM-yyyy (EEE)')
+)
+
+Vue.filter('formatISODate', (val) =>
+  format(parseISO(val), 'dd-MMM-yyyy (HH:mm)')
 )
