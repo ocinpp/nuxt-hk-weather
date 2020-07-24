2bc4b4d07212d329da705f2ee9ed91f43754f5ae
ocinpp <ocin25@gmail.com>
2020-07-24 10:03:09 +0800

    Add navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,19 @@
 <template>
-  <div>
-    <Nuxt />
+  <div class="pa3">
+    <div class="pa3">
+      <div class="w-100 mw8 center">
+        <header>
+          <nav class="flex w75 justify-around f4 pa1">
+            <nuxt-link class="link underline-hover dark-pink" to="/"
+              >Home</nuxt-link
+            >
+            <nuxt-link class="link underline-hover dark-pink" to="/district"
+              >District</nuxt-link
+            >
+          </nav>
+        </header>
+        <Nuxt />
+      </div>
+    </div>
   </div>
 </template>
