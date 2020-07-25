65f6499ec8d7f38ae8833d992c5a2e1213934952
ocinpp <ocin25@gmail.com>
2020-07-25 18:56:55 +0800

    Update text alignment of temperature info

--- components/CurrentWeather.vue
+++ components/CurrentWeather.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
     <div class="f2 mv3 cf">
-      <div class="w-30 w-10-l fl">
+      <div class="w-30 w-20-l fl tc">
         {{ weather.temperature.value }}
         {{ weather.temperature.unit }}
       </div>
-      <div class="w-70 w-90-l fl">
+      <div class="w-70 w-80-l fl">
         {{ weather.temperature.place }}
       </div>
     </div>
     <div class="mv3 cf">
-      <div class="w-30 w-10-l fl">
+      <div class="w-30 w-20-l fl tc">
         <div v-for="icon of weather.weatherIcons" :key="icon.index">
           <img width="90" :src="getImage(icon)" />
         </div>
@@ -18,7 +18,7 @@
       <div
         v-for="message of weather.warningMessages"
         :key="message.index"
-        class="w-70 w-90-l f4 fl"
+        class="w-70 w-80-l f4 fl"
       >
         {{ message }}
       </div>
