e2fd2177aaf6c3033859fc0d381adcd88add1e61
ocinpp <ocin25@gmail.com>
2020-07-25 15:32:58 +0800

    Add "fixed" top menu style

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,19 +1,28 @@
 <template>
   <div class="pa3">
-    <div class="pa3">
+    <div>
       <div class="w-100 mw8 center">
-        <header>
-          <nav class="flex w75 justify-around f4 pa1">
+        <header class="bg-black-90 fixed w-100 mw8 menu ph3 pv3">
+          <nav class="flex w75 justify-around f5 pa1 bg-dark-purple">
             <nuxt-link class="link underline-hover dark-pink" to="/"
               >Home</nuxt-link
+            >
+            <nuxt-link class="link underline-hover dark-pink" to="/forecast"
+              >Forecast</nuxt-link
             >
             <nuxt-link class="link underline-hover dark-pink" to="/district"
               >District</nuxt-link
             >
           </nav>
         </header>
-        <Nuxt />
+        <Nuxt class="pt4" />
       </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+.menu {
+  top: 0;
+}
+</style>
