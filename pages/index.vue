c0636191b0f441bc0f4435a854e900f49ab166af
ocinpp <ocin25@gmail.com>
2020-07-25 15:33:39 +0800

    Update index page to show current weather

--- pages/index.vue
+++ pages/index.vue
@@ -6,16 +6,10 @@
     <div class="pv1 tr">
       <div class="f7">Last updated: {{ updateDateTime | formatISODate }}</div>
     </div>
-    <current-temperature
+    <!-- <current-temperature
       :temperature="currentTemperature"
-    ></current-temperature>
-    <div class="pv2">
-      <h2 class="f4">{{ forecast }}</h2>
-    </div>
-    <div class="bt b--dark-pink">
-      <p class="lh-copy mv3">{{ generalSituation }}</p>
-    </div>
-    <forecast :weather-forecasts="weatherForecasts"></forecast>
+    ></current-temperature> -->
+    <current-weather :weather="currentWeather"></current-weather>
   </div>
 </template>
 
@@ -26,27 +20,25 @@ export default {
   // Also, the returned object will be merged with your data object
   async asyncData({ $axios }) {
     // https://www.hko.gov.hk/en/abouthko/opendata_intro.htm
-    const resForecast = await $axios.get(
-      'https://data.weather.gov.hk/weatherAPI/opendata/weather.php?dataType=fnd&lang=en'
-    )
-
     const resWeather = await $axios.get(
       'https://data.weather.gov.hk/weatherAPI/opendata/weather.php?dataType=rhrread&lang=en'
     )
 
+    const resCurrentWeather = await $axios.get(
+      'https://data.weather.gov.hk/weatherAPI/opendata/weather.php?dataType=flw&lang=en'
+    )
+
     return {
-      generalSituation: resForecast.data.generalSituation,
-      weatherForecasts: resForecast.data.weatherForecast,
       currentTemperature: resWeather.data.temperature.data.find(
         (x) => x.place === 'Hong Kong Observatory'
       ),
-      updateDateTime: resWeather.data.temperature.recordTime,
+      currentWeather: resCurrentWeather.data,
+      updateDateTime: resCurrentWeather.data.updateTime,
     }
   },
   data() {
     return {
       title: 'Hong Kong Weather',
-      forecast: '9-Day Weather Forecast',
     }
   },
 }
