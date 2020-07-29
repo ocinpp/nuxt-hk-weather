c9920fa13c0a28c0ebf4d467d6987e4570db8dd5
ocinpp <ocin25@gmail.com>
2020-07-29 20:44:15 +0800

    Update test on main page

--- test/e2e/specs/index.spec.js
+++ test/e2e/specs/index.spec.js
@@ -1,8 +1,17 @@
-const Page = require('../pageObjects/main.page')
+const Page = require('../pageObjects/current.page')
 
-describe('Example test', () => {
+describe('App Title Test', () => {
   it('should open correct app', () => {
     Page.open()
-    expect(browser).toHaveTitle('Example')
+    expect(browser).toHaveTitle('Hong Kong Weather')
+  })
+})
+
+describe('Page H1 Test', () => {
+  it('should have the correct H1', () => {
+    Page.open()
+    const header = $('h1')
+    expect(header).toBeExisting()
+    expect(header).toHaveText('Hong Kong Weather')
   })
 })
