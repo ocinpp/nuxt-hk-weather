8882357b5fce6af3c9ff9a7c301b7685657a70b4
ocinpp <ocin25@gmail.com>
2020-07-31 02:57:30 +0800

    Rearrange display of icon and warning message

--- components/CurrentWeather.vue
+++ components/CurrentWeather.vue
@@ -1,20 +1,20 @@
 <template>
   <div>
     <div class="f2 mv3 cf">
+      <div class="w-20 w-10-l fl tc">
+        <div v-for="icon of weather.weatherIcons" :key="icon.index">
+          <img width="80" :src="getImage(icon)" />
+        </div>
+      </div>
       <div class="w-30 w-20-l fl tc">
         {{ weather.temperature.value }}
         {{ weather.temperature.unit }}
       </div>
-      <div class="w-70 w-80-l fl">
+      <div class="w-50 w-70-l fl">
         {{ weather.temperature.place }}
       </div>
     </div>
     <div class="mv3 cf">
-      <div class="w-30 w-20-l fl tc">
-        <div v-for="icon of weather.weatherIcons" :key="icon.index">
-          <img width="90" :src="getImage(icon)" />
-        </div>
-      </div>
       <div class="w-70 w-80-l f4 fl">
         <div
           v-for="message of weather.warningMessages"
