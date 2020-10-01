4d32659d6324ef5901fbcd0e07d6a4ed2fc2a635
ocinpp <ocin25@gmail.com>
2020-10-01 20:06:29 +0800

    Add language files

---
+++ lang/en.js
@@ -0,0 +1,11 @@
+export default {
+  title: 'Hong Kong Weather Report',
+  menu_current: 'Current',
+  menu_forecast: 'Forecast',
+  menu_district: 'District',
+  general_situation: 'General Situation',
+  outlook: 'Outlook',
+  district_weather: 'District Weather',
+  nine_day_forecast: '9-Day Weather Forecast',
+  last_updated: 'Last updated',
+}
---
+++ lang/tc.js
@@ -0,0 +1,11 @@
+export default {
+  title: '香港天氣報告',
+  menu_current: '天氣實況',
+  menu_forecast: '天氣預報',
+  menu_district: '分區天氣',
+  general_situation: '天氣概況',
+  outlook: '展望',
+  district_weather: '分區天氣',
+  nine_day_forecast: '香港九天天氣預報',
+  last_updated: '最後更新',
+}
