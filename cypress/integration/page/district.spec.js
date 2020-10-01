030aaebbbde37351e97828274f240420d4d90088
ocinpp <ocin25@gmail.com>
2020-10-01 20:07:36 +0800

    Update cypress tests

--- cypress/integration/page/district.spec.js
+++ cypress/integration/page/district.spec.js
@@ -2,12 +2,12 @@
 
 // const BASE_URL = Cypress.env('base_url')
 
-describe('District Page Content Test', () => {
+describe('District (EN) Page Content Test', () => {
   const baseUrl = Cypress.config('baseUrl')
 
   it('visits "District" page', () => {
     cy.visit('/district')
-    cy.url().should('eq', baseUrl + '/district')
+    cy.url().should('eq', baseUrl + '/en/district')
   })
 
   it('has correct page heading', () => {
@@ -59,5 +59,65 @@ describe('District Page Content Test', () => {
       .children()
       .eq(0)
       .should('have.text', 'Tai Mei Tuk')
+  })
+})
+
+describe('District (TC) Page Content Test', () => {
+  const baseUrl = Cypress.config('baseUrl')
+
+  it('visits "District" page', () => {
+    cy.visit('/tc/district')
+    cy.url().should('eq', baseUrl + '/tc/district')
+  })
+
+  it('has correct page heading', () => {
+    cy.get('h1').should('have.text', '分區天氣')
+  })
+
+  it('has at least 26 locations', () => {
+    cy.get('#district-weather').children().should('have.length.at.least', 26)
+  })
+
+  it('has correct first district', () => {
+    cy.get('#district-weather')
+      .children()
+      .eq(0)
+      .children()
+      .eq(0)
+      .children()
+      .eq(0)
+      .should('have.text', '京士柏')
+  })
+
+  it('has correct first district temperature and unit', () => {
+    cy.get('#district-weather')
+      .children()
+      .eq(0)
+      .children()
+      .eq(0)
+      .children()
+      .eq(1)
+      .invoke('text')
+      .should('match', /\s*[0-9]{1,} C\s*/g)
+  })
+
+  it('has correct district temperatures and units', () => {
+    cy.get('#district-weather')
+      .children()
+      .each(($child) => {
+        const t = $child.children().eq(0).children().eq(1).text()
+        expect(t).match(/\s*[0-9]{1,} C\s*/g)
+      })
+  })
+
+  it('has correct last district', () => {
+    cy.get('#district-weather')
+      .children()
+      .last()
+      .children()
+      .eq(0)
+      .children()
+      .eq(0)
+      .should('have.text', '大美督')
   })
 })
--- cypress/integration/ui/navigation.spec.js
+++ cypress/integration/ui/navigation.spec.js
@@ -1,26 +1,51 @@
 /// <reference types="cypress" />
 
-describe('Navigation Menu Test', () => {
+describe('Navigation (EN) Menu Test', () => {
   const baseUrl = Cypress.config('baseUrl')
 
   it('clicks "Forecast" navigates to "Forecast" url', () => {
     cy.visit('/')
     cy.contains('Forecast').click()
-    cy.url().should('eq', baseUrl + '/forecast')
-    cy.location('pathname').should('eq', '/forecast')
+    cy.url().should('eq', baseUrl + '/en/forecast')
+    cy.location('pathname').should('eq', '/en/forecast')
   })
 
   it('clicks "District" navigates to "District" url', () => {
     cy.visit('/')
     cy.contains('District').click()
-    cy.url().should('eq', baseUrl + '/district')
-    cy.location('pathname').should('eq', '/district')
+    cy.url().should('eq', baseUrl + '/en/district')
+    cy.location('pathname').should('eq', '/en/district')
   })
 
   it('clicks "Current" navigates to "Current" url', () => {
     cy.visit('/district')
     cy.contains('Current').click()
-    cy.url().should('eq', baseUrl + '/')
-    cy.location('pathname').should('eq', '/')
+    cy.url().should('eq', baseUrl + '/en')
+    cy.location('pathname').should('eq', '/en')
+  })
+})
+
+describe('Navigation (TC) Menu Test', () => {
+  const baseUrl = Cypress.config('baseUrl')
+
+  it('clicks "Forecast" navigates to "Forecast" url', () => {
+    cy.visit('/tc')
+    cy.contains('天氣預報').click()
+    cy.url().should('eq', baseUrl + '/tc/forecast')
+    cy.location('pathname').should('eq', '/tc/forecast')
+  })
+
+  it('clicks "District" navigates to "District" url', () => {
+    cy.visit('/tc')
+    cy.contains('分區天氣').click()
+    cy.url().should('eq', baseUrl + '/tc/district')
+    cy.location('pathname').should('eq', '/tc/district')
+  })
+
+  it('clicks "Current" navigates to "Current" url', () => {
+    cy.visit('/tc/district')
+    cy.contains('天氣實況').click()
+    cy.url().should('eq', baseUrl + '/tc')
+    cy.location('pathname').should('eq', '/tc')
   })
 })
