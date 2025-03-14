85b8ef561029157ae2abc19298fdbe68cf16dde4
ocinpp <ocin25@gmail.com>
2025-03-15 01:26:48 +0800

    Migrate eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,4 +3,15 @@ import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt(
   // Your custom configs here
+  {
+    // vue files
+    files: ['**/*.vue'],
+    rules: {
+      'vue/html-self-closing': 'off',
+    },
+  },
+  {
+    // ignore files in cypress directory
+    ignores: ['**/cypress/**'],
+  }
 )
